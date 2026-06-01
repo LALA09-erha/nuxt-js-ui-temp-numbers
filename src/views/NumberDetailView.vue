722b337d90e434e9feeab2e39458378e8635db71
Fikri Ainun Najib <fikri41153.pesonaxii@gmail.com>
2026-06-01 18:52:47 +0700

    fix : view detail height

--- src/views/NumberDetailView.vue
+++ src/views/NumberDetailView.vue
@@ -215,7 +215,7 @@ onMounted(() => {
   cursor: help;
 }
 .number-detail {
-  min-height: 200vh;
+  min-height: 100vh;
   background: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
   padding: 20px;
 }
